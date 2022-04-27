36cf21adba3f3851c9b2f727cdf727151a7ae282
WRoeder <77704216+WRoeder@users.noreply.github.com>
2022-04-27 16:21:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Random_Name_Selector
+ Project to select a random name from a given list.
---
+++ android/app/src/main/kotlin/com/wroeder/com/random_name_selector/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wroeder.com.random_name_selector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
